f8840c388f0be7781372d489c6fe2c7e27356df7
Ron Welch <mmcelt@yahoo.com>
2020-06-10 14:12:27 -0600

    14 - Lock Cursor to Screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{	
+	#region Fields
+
+	
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
