d00b4ed4b065ddc43dbc4ad1d6aa33631aafc4e3
Ron Welch <mmcelt@yahoo.com>
2020-06-10 14:05:07 -0600

    13 - Making the Camera Follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Transform _target;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void LateUpdate() 
+	{
+		transform.position = _target.position;
+		transform.rotation = _target.rotation;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
