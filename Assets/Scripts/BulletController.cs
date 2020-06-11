a66d901e9aeb0e5aed31e78f469f41222f12d1a5
Ron Welch <mmcelt@yahoo.com>
2020-06-11 09:18:42 -0600

    22 - Moving the Projectile

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _moveSpeed;
+	[SerializeField] Rigidbody _theRB;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		_theRB.velocity = transform.forward * _moveSpeed;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
