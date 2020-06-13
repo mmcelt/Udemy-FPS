52d6f62629f4ae56975b6848bbc25fb880ddb807
Ron Welch <mmcelt@yahoo.com>
2020-06-13 14:39:02 -0600

    33 - Making the Enemy Chase & Lose the Player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,10 @@ public class EnemyController : MonoBehaviour
 
 	[SerializeField] float _moveSpeed;
 	[SerializeField] Rigidbody _theRB;
+	[SerializeField] float _distanceToChase = 10f, _distanceToLose = 15f;
+
+	bool _chasing;
+	Vector3 _targetPoint;
 
 	#endregion
 
@@ -20,8 +24,26 @@ public class EnemyController : MonoBehaviour
 	
 	void Update() 
 	{
-		transform.LookAt(PlayerController.Instance.transform.position);
-		_theRB.velocity = transform.forward * _moveSpeed;
+		_targetPoint = PlayerController.Instance.transform.position;
+		_targetPoint.y = transform.position.y;
+
+		if (!_chasing)
+		{
+			if(Vector3.Distance(transform.position, _targetPoint) < _distanceToChase)
+			{
+				_chasing = true;
+			}
+		}
+		else
+		{
+			transform.LookAt(_targetPoint);
+			_theRB.velocity = transform.forward * _moveSpeed;
+
+			if(Vector3.Distance(transform.position, _targetPoint) >= _distanceToLose)
+			{
+				_chasing = false;
+			}
+		}
 	}
 	#endregion
 
